60028701ab4595b62f37a05013811ccd065f9752
Dhia14675 <dhiariboz@gmail.com>
2022-09-15 10:45:23 +0700

    first time

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Biodata Diri" << endl;
+    cout << "==========================================" <<endl;
+    cout << "Nama   :Dhia Tawakalna" << endl;
+    cout << "NIM    :A11.2022.14675" << endl;
+    cout << "Alamat :Perumahan Ambarawa Asri RT06 RW12" << endl;
+    cout << "TTL    :Kabupaten Semarang,05 Mei 2004" << endl;
+    cout << "Hobi   :Bermain Futsal" << endl;
+    cout << "==========================================" <<endl;
+    return 0;
+}
